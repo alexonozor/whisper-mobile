1460cfb9b5804f9aacd724524614d01d93000a63
Ekeh Esom <mailesom@gmail.com>
2017-11-19 07:07:02 +0100

    hide notifactions is receiver & sender are the same

--- src/pages/user-notifications/user-notifications.ts
+++ src/pages/user-notifications/user-notifications.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, LoadingController } from 'ionic-angular';
 import { NotificationProvider } from '../../providers/notification/notification';
-import { AuthenticationProvider } from '../../providers/authentication/authentication'
+import { AuthenticationProvider } from '../../providers/authentication/authentication';
+import { UserProvider } from '../../providers/user/user';
 import { AssesmentResponsePage } from '.././assesment/assesment-response/assesment-response';
 
 /**
@@ -25,6 +26,7 @@ export class UserNotificationsPage {
     public navParams: NavParams, 
     public _notification: NotificationProvider,
     public _auth: AuthenticationProvider,
+    public _userService: UserProvider,
     public loadingCtrl: LoadingController
   ) {
     this.currentUser = this._auth.currentUser();
@@ -46,11 +48,31 @@ export class UserNotificationsPage {
     .subscribe((res) => {
       loading.dismiss();
       this.notifications = res.notifications;
-      if(this.notifications = []) {
+      console.log('notifications ', this.notifications);
+      this.findSender(this.notifications);
+      if(this.notifications == [] || null) {
+        this.no_notification = true;
+        console.log('no notifications ', this.no_notification);
+      }
+    }, err => {})
+  }
+
+  findSender(notifications){
+    let names: String;
+    this.notifications.forEach( notification => {
+      if(notification.sender['_id'] != null || '') {
+        this._userService.getUser(notification.sender['_id'])
+        .subscribe((res) => {
+          console.log('response ', res.user.firstName);
+          names = `${res.user.firstName} ${res.user.lastName}`;
+          notification['sender_names'] = names;
+        }, err => {});
+      };
+      // this adds a field to the response to signal if sender & receiver are the same, then filters notification
+      if( notification.sender['_id'] == notification.receiver['_id'] ) {
+        notification['hide_notification'] = true;
         this.no_notification = true;
       }
-    }, err => {
-      
     })
   }
 
