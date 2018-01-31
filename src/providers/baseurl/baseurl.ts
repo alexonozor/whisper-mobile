399164c18f1282a98758364445beee01f5d52b95
alexonozor <alexonozor@gmail.com>
2018-01-31 04:46:47 +0100

    fix location access

--- src/providers/baseurl/baseurl.ts
+++ src/providers/baseurl/baseurl.ts
@@ -17,7 +17,7 @@ export class BaseurlProvider {
               break;
            case 'localhost':
               data = {
-                endPoint: 'https://whisper-admin.herokuapp.com'
+                endPoint: 'http://localhost:7777'
               };
               break;
 
